e8e8c5d091e11ddaa42e8fe29d3da068e60ea4fd
krzysztoffila <umowy.fila@gmail.com>
2023-02-26 09:16:40 +0100

    Change bgcolors on mousemove

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const h1 = document.querySelector('h1');
+
 document.body.addEventListener('mousemove', event => {
   h1.innerHTML = `${event.clientX}, ${event.clientY}`;
   // h1.innerHTML = `${event.screenX}, ${event.screenY}`;
   // h1.textContent = `${event.pageX}, ${event.pageY}`;
   // h1.innerText = event.clientX + ", " + event.clientY;
+  let r = Math.floor(Math.random() * 255)
+  let g = Math.floor(Math.random() * 255)
+  let b = Math.floor(Math.random() * 255)
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`
+  console.log(change)
 })
