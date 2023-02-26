49419cabcaaf0402df8ae571e8198a410aab2229
krzysztoffila <umowy.fila@gmail.com>
2023-02-26 09:45:17 +0100

    Cleaning structure

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const h1 = document.querySelector('h1');
 
-document.body.addEventListener('mousemove', event => {
-  h1.innerHTML = `${event.clientX}, ${event.clientY}`;
-  // h1.innerHTML = `${event.screenX}, ${event.screenY}`;
-  // h1.textContent = `${event.pageX}, ${event.pageY}`;
-  // h1.innerText = event.clientX + ", " + event.clientY;
-  let r = Math.floor(Math.random() * 255)
-  let g = Math.floor(Math.random() * 255)
-  let b = Math.floor(Math.random() * 255)
-  document.body.style.backgroundColor = `rgb(${r},${g},${b})`
-  console.log(change)
+document.body.addEventListener('mousemove', e => {
+  /* MouseEvent.clientX && MouseEvent.clientY -> pozycja myszki wzgledem osi X i Y */
+  const x = e.clientX
+  const y = e.clientY
+  h1.innerHTML = `${x}, ${y}`;
+  /* window.innerWidth && window.innerHeigth -> w zaleznosci od okna przegladarki do 255(zeby bylo plynnie) */
+  const width = window.innerWidth
+  const height = window.innerHeight
+  // console.log(Math.floor(x / width * 255))
+  // console.log(Math.floor(y / height * 255))
+
+  /* implementacja kolorow */
+  const red = Math.floor(x / width * 255)
+  const green = Math.floor(y / width * 255);
+  const blue = Math.floor((x / width * 255) / Math.floor(y / width * 255))
+  document.body.style.backgroundColor = `rgb(${red}%,${green}%,${blue}%)`
 })
