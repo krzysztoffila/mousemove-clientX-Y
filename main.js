b802eda2bc627a25c123181ab49dae1a1efb3a64
krzysztoffila <umowy.fila@gmail.com>
2023-02-26 09:52:52 +0100

    HAVE FUN

--- main.js
+++ main.js
@@ -2,18 +2,16 @@ const h1 = document.querySelector('h1');
 
 document.body.addEventListener('mousemove', e => {
   /* MouseEvent.clientX && MouseEvent.clientY -> pozycja myszki wzgledem osi X i Y */
-  const x = e.clientX
-  const y = e.clientY
+  const x = e.clientX + 1;
+  const y = e.clientY + 1;
   h1.innerHTML = `${x}, ${y}`;
   /* window.innerWidth && window.innerHeigth -> w zaleznosci od okna przegladarki do 255(zeby bylo plynnie) */
   const width = window.innerWidth
   const height = window.innerHeight
-  // console.log(Math.floor(x / width * 255))
-  // console.log(Math.floor(y / height * 255))
 
   /* implementacja kolorow */
-  const red = Math.floor(x / width * 255)
-  const green = Math.floor(y / height * 255);
-  const blue = Math.floor((x / width * 255) / Math.floor(y / height * 255))
+  const red = x / width * 100;
+  const green = y / height * 100;
+  const blue = ((x / width * 100) + (y / height * 100)) / 2;
   document.body.style.backgroundColor = `rgb(${red}%,${green}%,${blue}%)`
 })
